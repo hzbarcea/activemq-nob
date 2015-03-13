43d98883128c5cdfabd0e06b08e012ea0bd41981
Ciprian Ciubotariu <cheepeero@gmx.net>
2015-03-14 00:43:03 +0200

    Add broker status constants for transition states
    These statuses should be used during the deployment/undeployment of
    broker instances.

--- activemq-nob-api/src/main/java/org/apache/activemq/nob/ActiveMQNobConstants.java
+++ activemq-nob-api/src/main/java/org/apache/activemq/nob/ActiveMQNobConstants.java
@@ -12,6 +12,8 @@ public final class ActiveMQNobConstants {
 	public static final String STATUS_UNKNOWN = "UNKNOWN";
 	public static final String STATUS_NEW = "NEW";
 	public static final String STATUS_ACTIVE = "ACTIVE";
+        public static final String STATUS_COMMISSIONED = "COMMISSIONED";
+        public static final String STATUS_DECOMMISSIONED = "DECOMMISSIONED";
 
 	private ActiveMQNobConstants() {
 		// Utility
