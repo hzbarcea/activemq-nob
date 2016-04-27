f5889c4e19cdf9e4d43971b961716e101030cc38
Ciprian Ciubotariu <cheepeero@gmx.net>
2016-04-28 00:57:11 +0300

    Implement karaf shell commands
    Implement minimal set of commands for broker management within karaf

--- activemq-nob-commands/src/main/java/org/apache/activemq/nob/karaf/command/List.java
+++ activemq-nob-commands/src/main/java/org/apache/activemq/nob/karaf/command/List.java
@@ -1,27 +1,40 @@
-
 package org.apache.activemq.nob.karaf.command;
 
+import org.apache.activemq.nob.api.Broker;
+import org.apache.activemq.nob.persistence.api.BrokerConfigurationServerPersistenceApi;
 import org.apache.karaf.shell.api.action.Action;
-import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
-import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.apache.karaf.shell.support.table.ShellTable;
 
-@Command(scope = "nob", name = "list", description = "Lists brokers in the nob")
+@Command(scope = "nob", name = "list", description = "Lists brokers")
 @Service
 public class List implements Action {
 
-    @Option(name = "-o", aliases = { "--option" }, description = "An option to the command", required = false, multiValued = false)
-    private String option;
-
-    @Argument(name = "argument", description = "Argument to the command", required = false, multiValued = false)
-    private String argument;
+    @Reference
+    BrokerConfigurationServerPersistenceApi serverPersistenceApi;
 
     @Override
     public Object execute() throws Exception {
-         System.out.println("Executing command list");
-         System.out.println("Option: " + option);
-         System.out.println("Argument: " + argument);
-         return null;
+        Iterable<Broker> brokerList = this.serverPersistenceApi.retrieveBrokerList();
+
+        ShellTable table = new ShellTable();
+        table.column("Entry ID");
+        table.column("Broker name");
+        table.column("Configuration date");
+        table.column("Status");
+        table.column("Broker URL");
+        for (Broker broker : brokerList) {
+            table.addRow().addContent(
+                    broker.getId(),
+                    broker.getName(),
+                    broker.getLastModifiedXbean().getTime(),
+                    broker.getStatus(),
+                    broker.getBrokerUrl());
+        }
+        table.print(System.out);
+
+        return null;
     }
 }
